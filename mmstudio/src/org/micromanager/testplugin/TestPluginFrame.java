a1b4f36ee0c139ed717ba05b85fd27ffaa114365
Gong <gxyjoe@hotmail.com>
2018-09-12 14:17:01 -0700

    Switch between two config files

--- mmstudio/src/org/micromanager/testplugin/TestPluginFrame.java
+++ mmstudio/src/org/micromanager/testplugin/TestPluginFrame.java
@@ -9,38 +9,33 @@ import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
-import java.text.NumberFormat;
-import java.util.List;
 import java.util.prefs.Preferences;
 import javax.swing.*;
 
-import mmcorej.CMMCore;
 import net.miginfocom.swing.MigLayout;
 import org.micromanager.MMStudio;
 import org.micromanager.Studio;
-import org.micromanager.api.ScriptInterface;
-import org.micromanager.data.Datastore;
-import org.micromanager.data.Image;
-import org.micromanager.internal.utils.MMFrame;
 import org.micromanager.utils.FileDialogs;
 import org.micromanager.utils.MMDialog;
-import org.micromanager.utils.MMException;
-import org.micromanager.utils.ReportingUtils;
+import org.micromanager.utils.*;
 
 public class TestPluginFrame extends MMDialog {
     
     private Studio studio_;
-    private JTextField userText_;
+    private JTextField firstUserText_;
+    private JTextField secondUserText_;
     private JLabel imageInfoLabel_;
     private MMDialog mscPluginWindow;
     private final Preferences prefs_;
     
-    private static final String LOWRESIMAGENAME = "Low resolution image";
-    private static final String EMPTY_FILENAME_INDICATOR = "None";
-    private String lowResFileName_;
+
+
+
     private final String[] IMAGESUFFIXES = {"tif", "tiff", "jpg", "png"};
-    private static File lowResImage;
-    
+    //private static File lowResImage;
+    private File firstConfig;
+    private File secondConfig;
+
     public TestPluginFrame(Studio studio) {
         super("Example Plugin GUI");
         studio_ = studio;
@@ -52,62 +47,87 @@ public class TestPluginFrame extends MMDialog {
         add(title, "span, alignx center, wrap");
         
         // Display the path to the low resolution image
-        add(new JLabel("Low resolution image: "));
+        add(new JLabel("First config file: "));
         
-        userText_ = new JTextField(30);
-        userText_.setText("Something happened!");
-        add(userText_);
+        firstUserText_ = new JTextField(30);
+        firstUserText_.setText("First Config File");
+        add(firstUserText_);
         
         // Create load button for the low resolution image
-        JButton lowResButton = new JButton(" ... ");
+        JButton firstButton = new JButton(" ... ");
         // Clicking on this button will invoke the ActionListener, which in turn
         // will ask the user to select a image.
-        lowResButton.addActionListener(new ActionListener() {
+        firstButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
             // 
-                lowResImage = FileDialogs.openFile(mscPluginWindow, 
-                        "Low resolution image", 
-                        new FileDialogs.FileType("MMAcq", "Low resolution image", 
-                                lowResFileName_, true, IMAGESUFFIXES));
-                if (lowResImage != null){
-                    processLowResImage(lowResImage.getAbsolutePath());
-                    userText_.setText(lowResFileName_);
+                firstConfig = loadConfiguration(firstUserText_);
+                //firstUserText_.setText("First file");
+                /*
+                firstConfig = FileDialogs.openFile(mscPluginWindow, 
+                        "Config file", 
+                        new FileDialogs.FileType("MMAcq", ".cfg", 
+                                firstConfigFileName_, true, IMAGESUFFIXES));
+                if (firstConfig != null){
+                    processLowResImage(firstConfig.getAbsolutePath());
+                    firstUserText_.setText(firstConfigFileName_);
                 }
+                */
             }
         });
-        add(lowResButton, "wrap");
+        add(firstButton, "wrap");
         
         // Add second config file 
                 // Display the path to the low resolution image
-        add(new JLabel("Low resolution image: "));
+        add(new JLabel("Second config file: "));
         
-        userText_ = new JTextField(30);
-        userText_.setText("Something happened!");
-        add(userText_);
+        secondUserText_ = new JTextField(30);
+        secondUserText_.setText("Second Config File");
+        add(secondUserText_);
         
         // Create load button for the low resolution image
-        JButton secondFile = new JButton(" ... ");
+        JButton secondButton = new JButton(" ... ");
         // Clicking on this button will invoke the ActionListener, which in turn
         // will ask the user to select a image.
-        secondFile.addActionListener(new ActionListener() {
+        secondButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
- 
+                secondConfig = loadConfiguration(secondUserText_);
             }
         });
-        add(secondFile, "wrap");
+        add(secondButton, "wrap");
         
         imageInfoLabel_ = new JLabel();
         add(imageInfoLabel_, "growx, split, span");
+        
+        JButton config1Button = new JButton("Config 1");
+        config1Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                chooseConfig(firstConfig);
+            }
+        });
+        add(config1Button);
+        
+        JButton config2Button = new JButton("Config 2");
+        config2Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                chooseConfig(secondConfig);
+            }
+        });
+        add(config2Button);
+        
+        /*
         JButton annotateButton = new JButton("Snap Image");
         annotateButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                userText_.setText("test");
+                secondUserText_.setText("test");
             }
         });
         add(annotateButton, "wrap");
+        */
         
         //retrieve the topmost DisplayWindow and then extract the Datastore that 
         //contains the data that the DisplayWindow presents
@@ -115,6 +135,7 @@ public class TestPluginFrame extends MMDialog {
         pack();
     }
     
+    /*
     public String processLowResImage(String fileName) {
         if (EMPTY_FILENAME_INDICATOR.equals(fileName)) {
             fileName = "";
@@ -123,18 +144,38 @@ public class TestPluginFrame extends MMDialog {
         prefs_.put(LOWRESIMAGENAME, lowResFileName_);
         return fileName;
     }  
+    */
     
-    private void loadConfiguration() {
-      File configFile = FileDialogs.openFile(MMStudio.getFrame(), 
+    private File loadConfiguration(JTextField userText_) {
+        File configFile = FileDialogs.openFile(MMStudio.getFrame(), 
             "Load a config file", MMStudio.MM_CONFIG_FILE);
+        userText_.setText(configFile.getName());
+        
+        ReportingUtils.logMessage("Read in " + configFile.getName() + " as config file");
+        return configFile;
+      /*
       if (configFile != null) {
          try{
-         studio_.getCMMCore().loadSystemConfiguration(configFile.getAbsolutePath());
+            studio_.getCMMCore().loadSystemConfiguration(configFile.getAbsolutePath());
+            userText_.setText(configFile.getName());
          }
          catch(Exception e){
-             System.out.println("Error");
+             System.out.println(e.getStackTrace());
          }
       }
+*/
    }
+    
+    private void chooseConfig(File configFile){
+         if (configFile != null) {
+         try{
+            studio_.getCMMCore().loadSystemConfiguration(configFile.getAbsolutePath());
+            ReportingUtils.logMessage("Choose " + configFile.getName() + " as config file");
+         }
+         catch(Exception e){
+             System.out.println(e.getStackTrace());
+         }
+      }
+    }
 }
 
